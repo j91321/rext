663649e8290595b457df3bbe8976e91c48edc0b3
j91321 <jan.trencansky@gmail.com>
2015-12-21 21:51:34 +0100

    Dlink DIR-890L DIR-300 and others soapaction header command execution exploit.

--- core/io.py
+++ core/io.py
@@ -6,7 +6,7 @@
 import datetime
 import os
 import core.globals
-from interface.messages import print_error
+from interface.messages import print_error, print_yellow, print_red
 
 
 def writefile(stream, filename):
@@ -29,3 +29,36 @@ def writetextfile(text, filename):
             return dirpath
     except OSError:
         print_error("Unable to create directory")
+
+
+# Modified from http://stackoverflow.com/questions/3041986/python-command-line-yes-no-input
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        print_yellow(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print_red("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
